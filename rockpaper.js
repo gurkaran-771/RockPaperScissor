3d3f16d7782c1d10af3fbf07009fa0a9d0ef5ea4
gurkaran-771 <gurkaransinghk05@gmail.com>
2023-04-12 20:02:33 -0400

    Done with 1st Part of RPS.

--- rockpaper.js
+++ rockpaper.js
@@ -1,21 +1,21 @@
-
-function getComputerChoice(){
+let result;
+function getComputerChoice() {
   //math.random()*max no and math.floor is used to round up numbers otherwise 
   //answer will be given in decimals. we need 
 
   let randomNumber;
   let computerChoice;
-  randomNumber=  Math.floor(Math.random()*3);
-  
+  randomNumber = Math.floor(Math.random() * 3);
+
   //the if-else is to decide what computer chose based on the number produced on random between 0,1 or 2
-  if (randomNumber ==0){
-       return computerChoice =  'Rock'
+  if (randomNumber == 0) {
+    return computerChoice = 'Rock'
   }
-  else if (randomNumber==1) {
-      return computerChoice = 'Paper'
+  else if (randomNumber == 1) {
+    return computerChoice = 'Paper'
   }
   else {
-      return computerChoice =  'Scissor'
+    return computerChoice = 'Scissor'
   }
 
 
@@ -24,43 +24,69 @@ console.log(getComputerChoice())
 
 
 
-function singleRound (playerSelection, computerSelection) {
+function singleRound(playerSelection, computerSelection) {
+  //player input
+  let playerInput = prompt('What are you going for? Rock, Paper or Scissor?')
+  playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase()
+  console.log('The player chose ' + playerSelection)
+  //computerSelection is whatever the result of getComputerChoice function will be
+  computerSelection = getComputerChoice();
+  console.log(`the computer chose ${computerSelection}`)
 
-let playerInput= prompt ('What are you going for? Rock, Paper or Scissor?')
-playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase()
-console.log('The player chose '  + playerSelection)
-computerSelection = getComputerChoice()  ;
-console.log (`the computer chose ${computerSelection}`)
+  //if conditions to determine the winner of a single round
 
+  if (playerSelection == computerSelection) {
+    return result = 'Ends up in draw'
+  }
 
+  else if (playerSelection == 'Rock' && computerSelection == 'Scissor'
+    || playerSelection == 'Paper' && computerSelection == 'Rock'
+    || playerSelection == 'Scissor' && computerSelection == 'Paper') {
+    return result = 'You win'
+  }
 
-if (playerSelection == computerSelection) { 
- return  result = 'It\'s a draw'
+  else if (playerSelection != 'Rock' && playerSelection != 'Paper' && playerSelection != 'Scissor') {
+
+    return (false)
+  }
+  else { return result = 'You lose' }
+
 }
-  
- else if (playerSelection == 'Rock' && computerSelection == 'Scissor'
-  || playerSelection == 'Paper' && computerSelection == 'Rock'
-  || playerSelection == 'Scissor' && computerSelection == 'Paper') {
- return result  = 'You win'
- }
- 
-   else if (playerSelection != 'Rock' && playerSelection!=  'Paper' && playerSelection!= 'Scissor') {
-     
-     return false
-   }
-else {return result = 'You lose'}
-  
-}
-
-
 
 function game() {
-for (let i = 0; i < 5; i++) {
-  singleRound();
- ;
-}
-}
+  //initialising variables that will be incremented depending on if player wins or computer
+  let playerWin = 0;
+  let computerWin = 0;
+  let drawMatch = 0;
+  //for loop to run the singleRound() 5 times
+  for (let i = 0; i < 5; i++) {
+    singleRound()
+    //result variable is the value we get everytime we invoke singleRound()
+    //   if conditions where the number count goes up depending on win or lose 
+    if (result == 'You lose') {
+      console.log(computerWin++)
+    }
+    else if (result == 'You win') {
+      console.log(playerWin++)
+    }
+    else if (result == 'Ends up in draw') {
+      console.log(drawMatch++)
+    }}
+  
+    // if conditions that declare winner of the round after checking the total wins and losses
+    if (playerWin > computerWin ) {
+    console.log('Congratulations! You won the match')
+    }
+    else if (playerWin < computerWin) {
+      console.log('You lost. Better luck next time :(')
+    }
+    else {
+    console.log('The match is tied(')
 
-game();
+  }
+
+  }
 
 game()
+
+
